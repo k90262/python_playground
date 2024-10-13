777b213c0b710be14042ca92a6472f46783f7a40
Bill Ho <ycho.bill.lab@gmail.com>
2024-10-14 00:17:49 +0800

    [Instantiating Custom Classes] Modifying Instance Representation

--- python-3-classes-object-oriented-programming/Employee.py
+++ python-3-classes-object-oriented-programming/Employee.py
@@ -1,13 +1,31 @@
 class Employee:
-    def __init__(self):
-        self.name = "Ji-Soo"
-        self.__dict__["age"] = 38
-        self.__dict__["position"] = "developer"
-        self.__dict__["salary"] = 12000
+    def __init__(self, name, age, position, salary):
+        self.name = name
+        self.age = age
+        self.position = position
+        self.salary = salary
 
-e = Employee()
-print(e)
-print(e.__class__)
-print(e.__dict__)
+    def increase_salary(self, percent):
+        self.salary += self.salary * (percent / 100)
+
+    def __str__(self):
+        return f"{self.name} is {self.age} years old. Employee is a {self.position} with the salary of {self.salary}"
+
+    def __repr__(self):
+        return (
+            f"Employee("
+            f"name={repr(self.name)}, "
+            f"age={repr(self.age)}, "
+            f"position={repr(self.position)}, "
+            f"salary={repr(self.salary)})"
+        )
+employee1 = Employee("Ji-Soo", 38, "developer", 1200)
+employee2 = Employee("Lauren", 44, "tester", 1000)
+print(employee1)
+print(employee1.__class__)
+print(employee1.__dict__)
 print(__name__)
-print(e.name)
+print(employee1.name)
+
+print(eval(repr(employee1)))
+
