dd902fd80821ff2b21ab69943fa2ca5f9ea5db36
Bill Ho <ycho.bill.lab@gmail.com>
2024-12-22 00:22:20 +0800

    [Improving Test Coverage and Maintainability][Demo: Replacing an Assertion with Approvals] implemented refactor

--- supermarket_receipt/tests/test_supermarket.py
+++ supermarket_receipt/tests/test_supermarket.py
@@ -1,46 +1,23 @@
 import approvaltests
 import pytest
 
-from model_objects import Product, SpecialOfferType, ProductUnit
+from model_objects import SpecialOfferType
 from receipt_printer import ReceiptPrinter
-from shopping_cart import ShoppingCart
-from teller import Teller
-from tests.fake_catalog import FakeCatalog
 
 
-def test_no_discount():
-    catalog = FakeCatalog()
-    toothbrush = Product("toothbrush", ProductUnit.EACH)
-    catalog.add_product(toothbrush, 0.99)
-    apples = Product("apples", ProductUnit.KILO)
-    catalog.add_product(apples, 1.99)
-
-    teller = Teller(catalog)
+def test_no_discount(teller, cart, toothbrush, apples):
     teller.add_special_offer(SpecialOfferType.TEN_PERCENT_DISCOUNT, toothbrush, 10.0)
-
-    cart = ShoppingCart()
     cart.add_item_quantity(apples, 2.5)
 
     receipt = teller.checks_out_articles_from(cart)
 
-    receipt_as_string = ReceiptPrinter().print_receipt(receipt)
-    approvaltests.verify(receipt_as_string)
+    approvaltests.verify(ReceiptPrinter().print_receipt(receipt))
 
 
-def test_ten_percent_discount():
-    catalog = FakeCatalog()
-    toothbrush = Product("toothbrush", ProductUnit.EACH)
-    catalog.add_product(toothbrush, 0.99)
-    apples = Product("apples", ProductUnit.KILO)
-    catalog.add_product(apples, 1.99)
-
-    teller = Teller(catalog)
+def test_ten_percent_discount(teller, cart, toothbrush):
     teller.add_special_offer(SpecialOfferType.TEN_PERCENT_DISCOUNT, toothbrush, 10.0)
-
-    cart = ShoppingCart()
     cart.add_item_quantity(toothbrush, 2)
 
     receipt = teller.checks_out_articles_from(cart)
 
-    receipt_as_string = ReceiptPrinter().print_receipt(receipt)
-    approvaltests.verify(receipt_as_string)
+    approvaltests.verify(ReceiptPrinter().print_receipt(receipt))
