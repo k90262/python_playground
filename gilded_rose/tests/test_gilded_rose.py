8dd9e36f13099cd2499a10d094453ae5070ae3d3
Bill Ho <ycho.bill.lab@gmail.com>
2024-12-27 19:46:54 +0800

    [Improving Test Coverage and Maintainability][Demo: Combination Approvals] refactor via using combination approval

--- gilded_rose/tests/test_gilded_rose.py
+++ gilded_rose/tests/test_gilded_rose.py
@@ -1,22 +1,33 @@
 # -*- coding: utf-8 -*-
 import unittest
-
 import approvaltests
 
 from gilded_rose import Item, GildedRose
 
+def test_update_quality():
+    names = ["foo"]
+    sell_ins = [0]
+    qualitys = [0]
+
+    approvaltests.verify_all_combinations(
+        update_quality_for_item,
+        [
+            names,
+            sell_ins,
+            qualitys,
+        ],
+        formatter=update_quality_printer
+    )
+
+def update_quality_for_item(name, sell_in, quality):
+    item = Item(name, sell_in, quality)
+    items = [item]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    return item
+
+def update_quality_printer(args, result):
+    return f"{args} => {print_item(result)}"
 
 def print_item(item):
     return f"Item({item.name}, sell_in={item.sell_in}, quality={item.quality})"
-
-
-def test_update_quality():
-    item = Item("foo", 0, 0)
-    items = [item]
-    gilded_rose = GildedRose(items)
-
-    gilded_rose.update_quality()
-
-    approvaltests.verify(print_item(item))
-
-
