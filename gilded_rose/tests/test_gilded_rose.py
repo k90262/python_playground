9fa13c5268c81139733cd00f05d1014598169e8b
Bill Ho <ycho.bill.lab@gmail.com>
2024-12-27 20:25:41 +0800

    [Improving Test Coverage and Maintainability][Demo: Combination Approvals] added test case to improve coverage into 92-100%

--- gilded_rose/tests/test_gilded_rose.py
+++ gilded_rose/tests/test_gilded_rose.py
@@ -5,9 +5,9 @@ import approvaltests
 from gilded_rose import Item, GildedRose
 
 def test_update_quality():
-    names = ["foo"]
-    sell_ins = [0]
-    qualitys = [0]
+    names = ["foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"]
+    sell_ins = [0, 1, 7, 13]
+    qualitys = [0, 1, 6, 60]
 
     approvaltests.verify_all_combinations(
         update_quality_for_item,
@@ -27,7 +27,7 @@ def update_quality_for_item(name, sell_in, quality):
     return item
 
 def update_quality_printer(args, result):
-    return f"{args} => {print_item(result)}"
+    return f"{args} => {print_item(result)}\n"
 
 def print_item(item):
     return f"Item({item.name}, sell_in={item.sell_in}, quality={item.quality})"
