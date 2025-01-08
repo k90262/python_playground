678e1ad77b65427b5e479ee273808079c0122fb9
Bill Ho <ycho.bill.lab@gmail.com>
2025-01-08 18:39:16 +0800

    [Code That’s Difficult to Test][Demo: Slice Strategy for Hard-to-test Code] Done

--- ice_cream_forecasts/tests/test_forecasts.py
+++ ice_cream_forecasts/tests/test_forecasts.py
@@ -1,9 +1,19 @@
+import datetime
+
 import approvaltests
 
-from forecasts import print_sales_forecasts
+from forecasts import print_sales_forecasts_with_update_selection
+from scorer import IceCream
+import scorer
 
 
 def test_sales_forecasts(capsys):
-    print_sales_forecasts()
+    print_sales_forecasts_with_update_selection(
+        now=datetime.datetime.fromisoformat("2025-01-08 18:08:53.024945"),
+        update_selection=stub_update_selection
+    )
     output = capsys.readouterr()
-    assert output.out == ""
+    approvaltests.verify(output.out)
+
+def stub_update_selection():
+    scorer.flavour = IceCream.Vanilla
